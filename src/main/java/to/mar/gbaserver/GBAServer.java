352937bfbb9c5b8f9654ffee5b404c41af35d761
Rohan Bafna <rouhannb@protonmail.com>
2024-03-29 11:53:12 -0400

    Log commands to file

--- src/main/java/to/mar/gbaserver/GBAServer.java
+++ src/main/java/to/mar/gbaserver/GBAServer.java
@@ -4,10 +4,19 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Objects;
+import java.util.logging.*;
 
 public class GBAServer {
+    private static Logger logger = Logger.getLogger("to.mar.gbaserver");
+    private static FileHandler fh;
     public static void main(String[] args) {
         try {
+            fh = new FileHandler("GBAServer_log.txt");
+            fh.setFormatter(new SimpleFormatter());
+            logger.addHandler(fh);
+            logger.setLevel(Level.INFO);
+            logger.setUseParentHandlers(false);
+
             ServerSocket socket = new ServerSocket(2346);
 
             while (true) {
@@ -28,6 +37,9 @@ public class GBAServer {
 
                 inputStream.close();
                 client.close();
+
+                String cli = String.format(command, input);
+                logger.info(String.format("Running command: %s", cli));
 
                 Runtime.getRuntime().exec(String.format(command, input));
             }
